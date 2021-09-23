72c5c5fb73a311051e14bddcb1b5aed729a23d76
Eesha <Eeshajpatel@gmail.com>
2021-09-23 11:34:55 -0400

    Initial Commit

---
+++ Integration_c++/Integration_c__App.swift
@@ -0,0 +1,17 @@
+//
+//  Integration_c__App.swift
+//  Integration_c++
+//
+//  Created by Eesha on 2021-09-23.
+//
+
+import SwiftUI
+
+@main
+struct Integration_c__App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
